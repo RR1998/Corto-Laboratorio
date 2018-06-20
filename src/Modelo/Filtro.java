da26efbc45cfb5482272829d81ddcc0cdb8e0893
uca <uca>
2018-06-20 11:59:21 -0600

    agregando proyecto

---
+++ src/Modelo/Filtro.java
@@ -0,0 +1,111 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo;
+
+/**
+ *
+ * @author LN710Q
+ */
+public class Filtro {
+
+    private int id;
+    private String nombre;
+    private int AñoEstreno;
+    private String numeroPelicula;
+    private String Director;  
+    private String PaisProcedencia;
+    private String Clasificacion;
+    private boolean Exibicion;
+    
+    public Filtro(){
+    }
+
+    public Filtro(int id, String nombre, int AñoEstreno, String Director, String PaisProcedencia, boolean Exibicion, String numeroPelicula) {
+        this.id = id;
+        this.nombre = nombre;
+        this.AñoEstreno = AñoEstreno;
+        this.Director = Director;
+        this.PaisProcedencia = PaisProcedencia;
+        this.Exibicion = Exibicion;
+    }
+
+    public Filtro(String nombre, int AñoEstreno, String Director, String PaisProcedencia, String Clasificacion, boolean Exibicion, String NumeroPelicula) {
+        this.nombre = nombre;
+        this.AñoEstreno = AñoEstreno;
+        this.Director = Director;
+        this.PaisProcedencia = PaisProcedencia;
+        this.Clasificacion = Clasificacion;
+        this.Exibicion = Exibicion;
+    }
+    
+    public String getNombre() {
+        return nombre;
+    }
+    
+    public void setNombre(String nombre){
+        this.nombre=nombre;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+    public int getAñoEstreno() {
+        return AñoEstreno;
+    }
+
+    public void setAñoEstreno(int AñoEstreno) {
+        this.AñoEstreno = AñoEstreno;
+    }
+
+    public String getDirector() {
+        return Director;
+    }
+
+    public void setDirector(String Director) {
+        this.Director = Director;
+    }
+
+    public String getPaisProcedencia() {
+        return PaisProcedencia;
+    }
+    
+    public String getNumeroPelicula() {
+        return numeroPelicula;
+    }
+
+    public void setNumeroPelicula(String numeroPelicula) {
+        this.numeroPelicula = numeroPelicula;
+    }
+
+    public void setPaisProcedencia(String PaisProcedencia) {
+        this.PaisProcedencia = PaisProcedencia;
+    }
+
+    public String getClasificacion() {
+        return Clasificacion;
+    }
+
+    public void setClasificacion(String Clasificacion) {
+        this.Clasificacion = Clasificacion;
+    }
+
+    public boolean isExibicion() {
+        return Exibicion;
+    }
+
+    public void setExibicion(boolean Exibicion) {
+        this.Exibicion = Exibicion;
+    }
+    
+    
+}
+
+
---
+++ src/dao/FiltroDao.java
@@ -0,0 +1,129 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+import conexion.Conexion;
+import interfaces.metodos;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import Modelo.Filtro;
+//marca es nombre
+//stock es edad 
+/**
+ *
+ * @author LN710Q
+ */
+public class FiltroDao implements metodos<Filtro> {
+    private static final String SQL_INSERT="INSERT INTO movie(numeroPelicula,nombre, director ,pais ,clasificacion ,exibicion)VALUES(?,?,?,?,?,?)";
+    private static final String SQL_UPDATE="UPDATE movie SET nombre=?,clasificacion=?,excibicion=? WHERE numInscripcion=?";
+    private static final String SQL_DELETE="DELETE FROM movie WHERE numeroPelicula=?";
+    private static final String SQL_READ="SELECT * FROM movie WHERE numeroPelicula=?";
+    private static final String SQL_READALL="SELECT * FROM movie";
+    private static final Conexion con = Conexion.conectar();
+    @Override
+    public boolean create(Filtro g) {
+        PreparedStatement ps;
+        try{
+            ps= con.getCnx().prepareStatement(SQL_INSERT);
+            ps.setString(1,g.getNumeroPelicula());
+            ps.setString(2, g.getNombre());
+            ps.setString(3, g.getDirector());
+            ps.setString(4,g.getPaisProcedencia());
+            ps.setString(5, g.getClasificacion());
+            ps.setBoolean(6,g.isExibicion());
+            if(ps.executeUpdate()>0){
+                return true;
+            }            
+        }catch(SQLException ex){
+            System.out.println(ex.getMessage());
+            Logger.getLogger(FiltroDao.class.getName()).log(Level.SEVERE, null, ex);
+        }finally{
+            con.cerrarConexion();
+        }
+        return false;        
+    }
+
+    @Override
+    public boolean delete(Object key) {
+        PreparedStatement ps;
+        try{
+            ps=con.getCnx().prepareStatement(SQL_DELETE);
+            ps.setString(1, key.toString());
+            if(ps.executeUpdate()>0){
+                return true;
+            }            
+        }catch(SQLException ex){
+            System.out.println(ex.getMessage());
+            Logger.getLogger(FiltroDao.class.getName()).log(Level.SEVERE, null, ex);
+        }finally{
+            con.cerrarConexion();
+        }
+        return false;   
+    }
+
+    @Override
+    public boolean update(Filtro c) {
+        PreparedStatement ps;
+        try{
+            System.out.println(c.getNombre());
+            ps=con.getCnx().prepareStatement(SQL_UPDATE);
+            ps.setString(1, c.getNombre());
+            ps.setInt(2,c.getEdad());
+            ps.setBoolean(3,c.getExistencia());
+            if(ps.executeUpdate()>0){
+                return true;
+            }            
+        }catch(SQLException ex){
+            System.out.println(ex.getMessage());
+            Logger.getLogger(FiltroDao.class.getName()).log(Level.SEVERE, null, ex);
+        }finally{
+            con.cerrarConexion();
+        }
+        return false;   
+    }
+
+    @Override
+    public Filtro read(Object key) {
+        Filtro f=null;
+        PreparedStatement ps;
+        ResultSet rs;
+        try{
+            ps=con.getCnx().prepareStatement(SQL_READ);
+            ps.setString(1, key.toString());
+            rs=ps.executeQuery();
+            
+            while(rs.next()){
+                f=new Filtro(rs.getString(1), rs.getString(2),rs.getString(3), rs.getInt(4),rs.getString(5),rs.getBoolean(6));
+            }
+            rs.close();
+        }catch(SQLException ex){
+            System.out.println(ex.getMessage());
+            Logger.getLogger(FiltroDao.class.getName()).log(Level.SEVERE, null, ex);
+        }finally{
+            con.cerrarConexion();
+        }
+        return f;   
+    }
+
+    @Override
+    public ArrayList<Filtro> readAll() {
+        ArrayList<Filtro> all = new ArrayList<>();
+        Statement s;
+        ResultSet rs;
+        try{
+            s=con.getCnx().prepareStatement(SQL_READALL);
+        }catch(SQLException ex){
+            Logger.getLogger(FiltroDao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return all;
+    }
+}
+
+
